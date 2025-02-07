d9ac29fe3774cc44c17732349922660f5e742d5d
Daniel Garza <danielg272313@gmail.com>
2025-02-07 19:18:05 +0000

    solve ABC 388 A

---
+++ ABC 388/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    cout << string(1, s[0]) + "UPC";
+    return 0;
+}
